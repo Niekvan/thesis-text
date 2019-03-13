447fa1a275490ea3942cada077ec4ee3ac2bd97d
Niek <17721120+Niekvan@users.noreply.github.com>
2019-03-13 12:43:36 +0100

    Revert "inobounce library"
    This reverts commit 268b240f5e29a10eedd2808e2b52193c494ee6ea.

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,10 +45,6 @@ module.exports = {
     {
       src: '~/node_modules/lazysizes/lazysizes.js',
       mode: 'client'
-    },
-    {
-      src: '~/plugins/iNoBounce.js',
-      mode: 'client'
     }
   ],
 
